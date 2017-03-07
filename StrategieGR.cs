c3e1f40cc12e9a361d245c08593331c821973741
Mona Buisson-Fenet <mona.buissonfenet@me.com>
2017-03-07 18:12:24 +0100

    Added strategy for demonstration (big commented chunk at the end)

--- StrategieGR.cs
+++ StrategieGR.cs
@@ -34,7 +34,7 @@ namespace GR
             Tracage.Ecrire("Sortie de la zone de depart");
 
             Tourner(12);
-            SortieOK = AllerEn(341, Equipe == Couleur.Bleu ? 946 : 2054, Plateforme.sens.avancer) == etat.arrive;
+            bool SortieOK = AllerEn(341, Equipe == Couleur.Bleu ? 946 : 2054, Plateforme.sens.avancer) == etat.arrive;
             //on prend la convention Couleur.Bleu == zone de départ bleue, sinon zone de départ jaune ; en cm pour l'instant, à voir ! Attention aussi aux conventions de repère, ici on a pris y vers le bas et angle positif en sens horaire mais pas forcément vrai
             //rappeler à PED de coder etat car permet savoir si arrive/perdu... et adapter le nom en fonction de ce qu'il choisit
 
@@ -120,7 +120,6 @@ namespace GR
 
         private bool DeposerCylindres()
         {
-          CylindresRecup++; //int à créer pour en faire un indice
 
           Tracage.Ecrire("Depot des cylindres");
 
@@ -175,11 +174,116 @@ namespace GR
             Strategie.Ajouter(new ActionRobot(RecupererCylindre2, RecupererCylindresFusee, () => 100 - CylindresRecup, true));
             Strategie.Ajouter(new ActionRobot(DeposerCylindres, RecupererCylindre2, () => 100 - CylindresRecup -1, true));
 
-            
-            /*Strategie.Ajouter(new ActionRobot(DeplacerChateaux,
+
+            /*Exemple P14 :
+            Strategie.Ajouter(new ActionRobot(DeplacerChateaux,
             () => SortieOK && (DateTime.Now - InstantDebut).Ticks < new TimeSpan(0, 1, 30).Ticks,
             () => 99 - TentativesChateaux * 2, true));*/
         }
+
+
+
+
+
+
+
+
+
+
+
+
+        //CODE POUR FAIRE UNE DÉMO!!!!!!!!!!! (pas d'action des servos moteurs, seulement des mouvements)
+/*
+        private bool SortirZoneDepart()
+        {
+          Tracage.Ecrire("Sortie de la zone de depart");
+
+          Tourner(12);
+          bool SortieOK = AllerEn(341, Equipe == Couleur.Bleu ? 946 : 2054, Plateforme.sens.avancer) == etat.arrive;
+          //on prend la convention Couleur.Bleu == zone de départ bleue, sinon zone de départ jaune ; en cm pour l'instant, à voir ! Attention aussi aux conventions de repère, ici on a pris y vers le bas et angle positif en sens horaire mais pas forcément vrai
+          //rappeler à PED de coder etat car permet savoir si arrive/perdu... et adapter le nom en fonction de ce qu'il choisit
+
+          Tracage.Ecrire(SortieOK ? "Sortie reussie" : "Sortie echouee");
+          //si SortieOk == etat.arrive alors on écrit Sortie reussie, sinon on écrit Sortie echouee
+
+          return SortieOK;
+        }
+
+        private bool RecupererCylindre1()
+        {
+          CylindresRecup++; //À CODER (pas essentiel) : un entier haut niveau qui compte le nombre de cylindres déjà récupérés
+
+          Tracage.Ecrire("Recuperation du 1er cylindre");
+          Tourner(Equipe==Couleur.Bleu ? 78 : -78);
+          return true;
+        }
+
+        private bool RecupererCylindresFusee()
+        {
+          CylindresRecup+=4; //int à créer pour en faire un indice
+
+          Tracage.Ecrire("Positionnement devant la fusee et recuperation des 4 cylindres");
+
+          AllerEn(341,Equipe==Couleur.Bleu ? 1150 : 1850, Plateforme.sens.avancer);
+          Tourner(Equipe==Couleur.Bleu ? 90 : -90);
+          AllerEn(310, Equipe==Couleur.Bleu ? 1150 : 1850, Plateforme.sens.avancer);
+
+          return true;
+        }
+
+        private bool RecupererCylindre2()
+        {
+          CylindresRecup++; //int à créer pour en faire un indice
+
+          Tracage.Ecrire("Recuperation du 2eme cylindre");
+
+          AllerEn(850,1150, Equipe==Couleur.Bleu ? Plateforme.sens.avancer : Plateforme.sens.reculer);
+          Tourner(180);
+          Tourner(Equipe==Couleur.Bleu ? -45 : 45);
+          AllerEn(1100, Equipe==Couleur.Bleu ? 900 : 2100, Plateforme.sens.avancer);
+          Tourner(Equipe==Couleur.Bleu ? 45 : -45);
+          AllerEn(1136, Equipe==Couleur.Bleu ? 900 : 2100);
+
+          return true;
+        }
+
+        private bool DeposerCylindres()
+        {
+
+
+          Tracage.Ecrire("Depot des cylindres");
+
+          AllerEn(1125, Equipe==Couleur.Equipe ? 900 : 2100, Plateforme.sens.reculer);
+          Tourner(Equipe==Couleur.Bleu ? -45 : 45);
+          AllerEn(1275, Equipe==Couleur.Bleu ? 750 : 2250, Plateforme.sens.avancer);
+          Tourner(Equipe==Couleur.Bleu ? -90 : 90);
+          AllerEn(1427, Equipe==Couleur.Bleu ? 927 : 2073, Plateforme.sens.reculer);
+
+
+          return true;
+        }
+
+        private void InitialiserStrategie()
+        {
+
+            Strategie.Ajouter(new ActionRobot(SortirZoneDepart, executionUnique: true));
+            /*Permet de gérer l'enchaînement des actions : on ajoute une par une les actions qu'on veut exécuter, sous la forme
+            new ActionRobot(nom, condition, priorité, exécution unique (tous ces param sauf le nom sont optionnels)). La condition
+            est soit une fonction booléenne du type de celles au-dessus  (la suivante n'est exécutée que si la précédente a été appelée
+            avant), soit une condition temporelle du type ci-dessous dans le code P14*/
+/*
+            Strategie.Ajouter(new ActionRobot(RecupererCylindre1, SortirZoneDepart, () => 100-CylindresRecup, true));
+            Strategie.Ajouter(new ActionRobot(RecupererCylindresFusee, RecupererCylindre1, () => 100-CylindresRecup, true));
+            Strategie.Ajouter(new ActionRobot(RecupererCylindre2, RecupererCylindresFusee, () => 100 - CylindresRecup, true));
+            Strategie.Ajouter(new ActionRobot(DeposerCylindres, RecupererCylindre2, () => 100 - CylindresRecup -1, true));
+        }
+
+
+        */
+
+
+
+
 
 
 
