8be2b5b0f711ee6e101a5b7145a1d6a47c5b5de2
pedevineau <pierre-etienne.devineau@mines-paristech.fr>
2017-03-08 09:45:02 +0100

    Include CRouletteIntelligente

--- CReservoir.cs
+++ CReservoir.cs
@@ -5,35 +5,29 @@ namespace RobotWallE
 {
     class CReservoir
     {
-        const int PAS_DE_ROTATION = 10; 
-
         struct configReservoir
         {
-            public SerialPort portSerie;
             public byte idAx12Poussoir;
             public byte idAx12Rotateur;
-            public OutputPort direction;
             public byte idCapteurReservoir;
         };
 
         enum positionReservoir
         {
-          rentre = ,
-          deploye = ,
+          rentre = 10,
+          deploye = 10,
         };
 
         CAX_12 m_ax12Poussoir;
-        CAX_12 m_ax12Rotateur;
-        CCapteurCouleur m_capteurCouleur;
+        CRouletteIntelligente m_rouletteIntelligente;
 
         public CReservoir(ControleurAX12 controleur, configReservoir config)
         {
-            m_capteurCouleur = new CCapteurCouleur(config.idCapteurReservoir);
+            CCapteurCouleur capteurCouleur = new CCapteurCouleur(config.idCapteurReservoir);
+            CAX_12 ax12Rotateur = new CAX_12(config.idAx12Rotateur, controleur.m_port, controleur.m_direction);
+            m_rouletteIntelligente = CRouletteIntelligente(capteurCouleur, ax12Rotateur);
             m_ax12Poussoir = new CAX_12(config.idAx12Poussoir, controleur.m_port, controleur.m_direction);
-            m_ax12Rotateur = new CAX_12(config.idAx12Rotateur, controleur.m_port, controleur.m_direction);
-           // m_ax12Pince.setMode(CAX_12.AX12Mode.joint);
-            //position initiale
-            // m_ax12Pince.setMode(CAX_12.AX12Mode.joint);
+            m_ax12Poussoir.setMode(CAX_12.AX12Mode.joint);
         }
 
         public void rentrer(Couleur equipe)
@@ -54,10 +48,7 @@ namespace RobotWallE
 
         public void tourner(Couleur equipe)
         {
-            while (m_capteurCouleur.continuerRotation()) 
-            {
-                m_ax12Rotateur.move(PAS_DE_ROTATION);
-            }
+            m_rouletteIntelligente.mettreBonneCouleur(equipe);
         }
 
     }
